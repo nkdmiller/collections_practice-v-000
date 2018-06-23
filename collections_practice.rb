fc7d4566a3d9eb2aa12fa26362820a73abd96917
Nate Miller <natekmiller96@gmail.com>
2018-06-23 18:19:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,10 @@ def sort_array_char_count(array)
     a.length <=> b.length
   end
 end
+def swap_elements(array)
+  array.sort do |a, b|
+    if a = array[1] && b = array[2]
+      1
+    end
+  end
+end
